59c2c81bcdb66c81a6a18c9856e2640684a0d29a
levi <levidalrymple1221@gmail.com>
2025-04-26 10:00:14 -0500

    Reworks the debug pos format function

--- src/DebugPos.zig
+++ src/DebugPos.zig
@@ -14,19 +14,6 @@ column: u32 = 0,
 multi_line: bool = false,
 
 pub fn format(self: Self, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-    // // print orginal buffer + start - PEEK_SIZE unless it is negative
-    // // print orginal buffer + end + PEEK_SIZE unless it is greater than the length of the buffer or end of line
-    // // with squiggle lines under start + end
-    // const start = if (self.start <= PEEK_SIZE) 0 else self.start - PEEK_SIZE;
-    // const end = if (self.end + PEEK_SIZE >= self.orginal_buffer.len) self.orginal_buffer.len else self.end + PEEK_SIZE;
-    //
-    // // get how much space we need to get squiggle lines
-    // const width = self.end - self.start;
-    // const offset = self.start - start;
-    //
-    // try writer.print("{s}\n", .{self.orginal_buffer[start..end]});
-    // try writer.writeByteNTimes(' ', offset);
-    // try writer.writeByteNTimes('^', width);
     // PLEASE NOTE LINE CAN BE MULTIPLE LINES
     // first step: find the start of the line need
     const start_line = std.mem.lastIndexOfScalar(u8, self.orginal_buffer[0..self.start], '\n') orelse 0;
@@ -54,8 +41,8 @@ pub fn format(self: Self, comptime _: []const u8, _: std.fmt.FormatOptions, writ
         };
 
         // make sure to ignore the spaces
-        const new_start = if (cutoff_start) if (std.mem.lastIndexOfScalar(u8, self.orginal_buffer[0..start], ' ')) |index| index + 1 else 0 else start;
-        const new_end = if (cutoff_end) if (std.mem.indexOfScalar(u8, self.orginal_buffer[end..], ' ')) |index| index - 1 else self.orginal_buffer.len else end;
+        const new_start = if (cutoff_start) if (std.mem.lastindexofscalar(u8, self.orginal_buffer[0..start], ' ')) |index| index + 1 else 0 else start;
+        const new_end = if (cutoff_end) if (std.mem.indexofscalar(u8, self.orginal_buffer[end..], ' ')) |index| index - 1 else self.orginal_buffer.len else end;
 
         const text = self.orginal_buffer[new_start..new_end];
 
